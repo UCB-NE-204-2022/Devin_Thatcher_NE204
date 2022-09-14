5a324f48c18d66b550f5bd0ac9493e226b708c02
devthatcher0 <devthatcher@berkeley.edu>
2022-09-14 11:26:00 -0700

    plot now working

--- DevinMCA.py
+++ DevinMCA.py
@@ -1,9 +1,7 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
-channel = []
 counts = []
-x = 1
 
 filelocation = input("File Location:")
 filelocation = filelocation.strip('"')
@@ -14,11 +12,9 @@ with open(filelocation) as MCAfile:
         try:
             y = int(line)
             counts.append(y)
-            channel.append(x)
-            x += 1
             line = MCAfile.readline()
         except ValueError:
             line = MCAfile.readline()
 
-print(channel)
-print(counts)
+plt.plot(counts)
+plt.show()
