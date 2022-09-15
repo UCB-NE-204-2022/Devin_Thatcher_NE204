e0c626168cde3d231ca98c5ec5fec7b186a0c6a5
devthatcher0 <devthatcher@berkeley.edu>
2022-09-14 20:36:06 -0700

    legend working

--- DevinMCA.py
+++ DevinMCA.py
@@ -13,30 +13,36 @@ def getData(x):
                 line = MCAfile.readline()
     return counts
 
-filelocation = 1
+file = []
 unstackedData = []
 
 while True:
-    filelocation = input("File Location:")
+    filelocation = input("Enter file locations then 'done' to plot: ")
     filelocation = filelocation.strip('"')
     try:
         counts = np.array(getData(filelocation))
         unstackedData.append(counts)
+        file.append(filelocation)
     except:
-        break
+        if filelocation == 'done':
+            break
+        else:
+            print("Unable to load data")
 
 stackedData = np.vstack(unstackedData)
 arrayheight = np.size(stackedData, 0)
 
+plt.title('MCA Data')
 plt.figure(1)
 plt.subplot(211)
 for x in range(arrayheight):
-     plt.plot(stackedData[x])
+     plt.plot(stackedData[x], label = file[x])
 plt.xlabel('Channel')
 plt.ylabel('Counts')
+plt.legend()
 plt.subplot(212)
 for x in range(arrayheight):
-     plt.plot(stackedData[x])
+     plt.plot(stackedData[x], label = file[x])
 plt.xlabel('Channel')
 plt.ylabel('Counts')
 plt.yscale('log')
