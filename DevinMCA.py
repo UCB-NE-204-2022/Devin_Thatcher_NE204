0b3854206d48a564014fd8b85e878dbd60519b69
devthatcher0 <devthatcher@berkeley.edu>
2022-09-14 19:34:07 -0700

    multiple data sets now working

--- DevinMCA.py
+++ DevinMCA.py
@@ -1,28 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-counts = []
+def getData(x):
+    counts = []
+    with open(x) as MCAfile:
+        line = MCAfile.readline()
+        while line != '':
+            try:
+                counts.append(int(line))
+                line = MCAfile.readline()
+            except:
+                line = MCAfile.readline()
+    return counts
 
-filelocation = input("File Location:")
-filelocation = filelocation.strip('"')
+filelocation = 1
+unstackedData = []
 
-with open(filelocation) as MCAfile:
-    line = MCAfile.readline()
-    while line != '':
-        try:
-            y = int(line)
-            counts.append(y)
-            line = MCAfile.readline()
-        except ValueError:
-            line = MCAfile.readline()
+while True:
+    filelocation = input("File Location:")
+    filelocation = filelocation.strip('"')
+    try:
+        counts = np.array(getData(filelocation))
+        unstackedData.append(counts)
+    except:
+        break
+
+stackedData = np.vstack(unstackedData)
+arrayheight = np.size(stackedData, 0)
 
 plt.figure(1)
 plt.subplot(211)
-plt.plot(counts)
+for x in range(arrayheight):
+     plt.plot(stackedData[x])
 plt.xlabel('Channel')
 plt.ylabel('Counts')
 plt.subplot(212)
-plt.plot(counts)
+for x in range(arrayheight):
+     plt.plot(stackedData[x])
 plt.xlabel('Channel')
 plt.ylabel('Counts')
 plt.yscale('log')
