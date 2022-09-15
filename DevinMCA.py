648f5594e6dc1acef2d33c1805eb48a2ee000dd9
devthatcher0 <devthatcher@berkeley.edu>
2022-09-15 14:39:57 -0700

    option to plot individually or sum in either order

--- DevinMCA.py
+++ DevinMCA.py
@@ -13,11 +13,46 @@ def getData(x):
                 line = MCAfile.readline()
     return counts
 
+def individualplot(x):
+    stackedData = np.vstack(x)
+    arrayheight = np.size(stackedData, 0)
+    plt.title('MCA Data')
+    plt.figure(1)
+    plt.subplot(211)
+    for x in range(arrayheight):
+        plt.plot(stackedData[x], label = file[x])
+    plt.xlabel('Channel')
+    plt.ylabel('Counts')
+    plt.legend()
+    plt.subplot(212)
+    for x in range(arrayheight):
+        plt.plot(stackedData[x], label = file[x])
+    plt.xlabel('Channel')
+    plt.ylabel('Counts')
+    plt.yscale('log')
+    plt.show()
+
+def summedplot(x):
+    stackedData = np.vstack(x)
+    summeddata = np.sum(stackedData, axis=0)
+    plt.title('MCA Data')
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(summeddata)
+    plt.xlabel('Channel')
+    plt.ylabel('Counts')
+    plt.subplot(212)
+    plt.plot(summeddata)
+    plt.xlabel('Channel')
+    plt.ylabel('Counts')
+    plt.yscale('log')
+    plt.show()
+
 file = []
 unstackedData = []
 
 while True:
-    filelocation = input("Enter file locations then 'done' to plot: ")
+    filelocation = input("Enter file locations then 'done' to plot individually, or 'sum' to sum data then plot: ")
     filelocation = filelocation.strip('"')
     try:
         counts = np.array(getData(filelocation))
@@ -25,43 +60,10 @@ while True:
         file.append(filelocation)
     except:
         if filelocation == 'done':
-            break
+            individualplot(unstackedData)
+        elif filelocation == 'sum':
+            summedplot(unstackedData)
         else:
             print("Unable to load data")
 
-stackedData = np.vstack(unstackedData)
-arrayheight = np.size(stackedData, 0)
 
-plt.title('MCA Data')
-plt.figure(1)
-plt.subplot(211)
-for x in range(arrayheight):
-     plt.plot(stackedData[x], label = file[x])
-plt.xlabel('Channel')
-plt.ylabel('Counts')
-plt.legend()
-plt.subplot(212)
-for x in range(arrayheight):
-     plt.plot(stackedData[x], label = file[x])
-plt.xlabel('Channel')
-plt.ylabel('Counts')
-plt.yscale('log')
-plt.show()
-
-while True:
-    sumOption = input("Enter 'sum' to sum data and re-plot: ")
-    if sumOption == 'sum':
-        summeddata = np.sum(stackedData, axis=0)
-        plt.title('MCA Data')
-        plt.figure(1)
-        plt.subplot(211)
-        plt.plot(summeddata)
-        plt.xlabel('Channel')
-        plt.ylabel('Counts')
-        plt.subplot(212)
-        plt.plot(summeddata)
-        plt.xlabel('Channel')
-        plt.ylabel('Counts')
-        plt.yscale('log')
-        plt.show()
-        break
