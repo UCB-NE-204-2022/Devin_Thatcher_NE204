3c7850816f442624ee1cc4abf6017a2164b01436
devthatcher0 <devthatcher@berkeley.edu>
2022-09-28 22:08:21 -0700

    pulses are now trapezoids

--- MyCode/204/HDF5filterpulseview.py
+++ MyCode/204/HDF5filterpulseview.py
@@ -2,13 +2,37 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
+risetime = 150
+flattop = 200
+numbertoplot = 1000
+
 filelocation = input("Enter file location: ")
 filelocation = filelocation.strip('"')
 with h5py.File(filelocation, 'r') as f:
     data = np.array(f['raw_data'])
-    data = data[:, :3000]
-    arrayheight = np.size(data, 0)
-    for x in range(0,100):
-        plt.plot(data[x])
+    pulses = np.empty([numbertoplot, 3000])
+    for a in range(numbertoplot):
+        pulse = data[a, :3000]
+        size = np.size(pulse)
+        baseline = np.average(data[a, :900])
+        bpulse = []
+        cpulse = []
+        dpulse = [0]
+        for b in range(size):                       
+            bpulse.append(pulse[b] - baseline)
+        for b in range(size):
+            if b < risetime:
+                cpulse.append(bpulse[b])
+            elif risetime <= b <= risetime+flattop-1:
+                cpulse.append(bpulse[b]-bpulse[b-risetime])
+            elif risetime+flattop <= b <= 2*risetime+flattop-1:
+                cpulse.append(bpulse[b]-bpulse[b-risetime]-bpulse[b-(risetime+flattop)])
+            else:
+                cpulse.append(bpulse[b]-bpulse[b-risetime]-bpulse[b-(risetime+flattop)]+bpulse[b-(2*risetime+flattop)])
+        for b in range(1, size):
+            dpulse.append(dpulse[b-1]+cpulse[b])
+        pulses[a]=np.array(dpulse)
+    for x in range(0,numbertoplot):
+        plt.plot(pulses[x])
 
 plt.show()
--- MyCode/204/HDF5pulseview.py
+++ MyCode/204/HDF5pulseview.py
@@ -7,8 +7,7 @@ filelocation = filelocation.strip('"')
 with h5py.File(filelocation, 'r') as f:
     data = np.array(f['raw_data'])
     data = data[:, :3000]
-    arrayheight = np.size(data, 0)
-    for x in range(0,100):
+    for x in range(0,1000):
         plt.plot(data[x])
 
 plt.show()
