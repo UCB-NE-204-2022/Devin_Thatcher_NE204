4e73eca642d5a93611357cb5311d41d70ad2cefc
devthatcher0 <devthatcher@berkeley.edu>
2022-09-30 14:40:25 -0700

    input multiple spectra

--- MyCode/204/ViewHistogram.py
+++ MyCode/204/ViewHistogram.py
@@ -1,22 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-filelocation = input("Enter spectra location: ")
-filelocation = filelocation.strip('"')
-spectra = np.load(filelocation)
-filelocation = input("Enter spectra2 location: ")
-filelocation = filelocation.strip('"')
-spectra2 = np.load(filelocation)
-ratio = float(np.amax(spectra2))/float(np.amax(spectra))
-spectraratio = []
-for a in range(np.size(spectra)):
-    spectraratio.append(spectra2[a]/spectra[a])
-ratio = np.average(spectraratio)
-for a in range(np.size(spectra)):
-    spectra[a]=ratio*spectra[a]
-resolution = int(np.amax(spectra)/20)
-plt.hist(spectra, bins=resolution, histtype='step', label='raw_data max pulse height')
-plt.hist(spectra2, bins=resolution, histtype='step', label='after trapezoidal filter + pole zero correction')
+while True:
+    try:
+        filelocation = input("Enter spectra location: ")
+        filelocation = filelocation.strip('"')
+        spectra = np.load(filelocation)
+        resolution = int(np.amax(spectra)/20)
+        plt.hist(spectra, bins=resolution, histtype='step', label=filelocation)
+    except:
+        break
+
 plt.xlim(0)
 plt.legend(loc='upper left')
 plt.show()
