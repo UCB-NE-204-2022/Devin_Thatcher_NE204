ad49db63753cd3806682a808b88df01989e11999
devthatcher0 <devthatcher@berkeley.edu>
2022-10-09 12:25:01 -0700

    Everything working, renamed files

--- MyCode/204/EnergyCalibration.py
+++ MyCode/204/EnergyCalibration.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks, peak_widths
 import matplotlib.pyplot as plt
 from datetime import datetime
 
@@ -10,7 +10,7 @@ while True:
     if isotope == '':
         break
     hist = []
-    peaklocations = []
+    peaklocations2 = []
     peakheights = []
     energylist = []
     while True:
@@ -18,12 +18,17 @@ while True:
             filelocation = input("Enter location of calibration isotope spectra: ").strip('"')
             spectra = np.load(filelocation)
             hist, bins = np.histogram(spectra, bins = spectrarange//20, range = (0, spectrarange))
-            peaklocations, _ = find_peaks(hist, distance = 100, prominence = int(np.amax(hist))/10)
+            peaklocations, _ = find_peaks(hist, distance = 10, prominence = int(np.amax(hist))/15)
+            FWHM = peak_widths(hist, peaklocations, rel_height = 0.5)
             for x in range(np.size(peaklocations)):
-                peakheights.append(hist[int(peaklocations[x])])
+                if 5 < FWHM[0][x] < 20:
+                    print('X axis location: ' + str(peaklocations[x]) + ' | ' + str(hist[int(peaklocations[x])]) + ' counts | ' + str(FWHM[0][x]) + ' FWHM')
+                    peaklocations2.append(peaklocations[x])
+                    peakheights.append(hist[int(peaklocations[x])])
+            np_peaklocations2 = np.array(peaklocations2, dtype = int)   
             peakheights.sort(reverse=True)
             plt.plot(hist, label=filelocation)
-            plt.plot(peaklocations, hist[peaklocations], "vk")
+            plt.plot(np_peaklocations2, hist[np_peaklocations2], "vk")
             plt.xlim(0)
             plt.legend(loc='upper left')
             plt.show()
@@ -32,7 +37,7 @@ while True:
             print("Cannot load file")
     while True:
         try:
-            energy = input("Enter energies (in keV) of highest peaks indicated: ")
+            energy = input("Enter energies (in keV) of the tallest peaks indicated: ")
             energylist.append(float(energy))
         except:
             if energy == '':
@@ -44,11 +49,11 @@ while True:
     with open('calibrationdata.txt', 'a') as f:
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        f.write(isotope + ' ' + dt_string + '\n')
+        f.write('\n' + isotope + '\n' + dt_string + '\n')
         x = 0
-        for y in range(np.size(peaklocations)):
-            if hist[int(peaklocations[y])] in peakheights2:
-                f.write(str(peaklocations[y]) + ' ' + str(energylist[x]) + '\n')
+        for y in range(np.size(peaklocations2)):
+            if hist[int(peaklocations2[y])] in peakheights2:
+                f.write(str(peaklocations2[y]) + ' ' + str(energylist[x]) + '\n')
                 x += 1
             else:
                 pass
--- MyCode/204/ViewHistogram.py
+++ MyCode/204/ViewHistogram.py
@@ -1,24 +1,50 @@
 import numpy as np
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks, peak_widths
 import matplotlib.pyplot as plt
 
 spectrarange = 100000
+calfactor = 1
+
+calfilelocation = input("Enter calibration data .txt file location, or press enter to continue without calibrated energies: ").strip('"')
+if calfilelocation != '':
+    with open(calfilelocation, 'r') as calfile:
+        factorlist = []
+        for line in calfile:
+            try:
+                singlefactor = line.split()            
+                factorlist.append(float(singlefactor[0])/float(singlefactor[1]))
+            except:
+                pass
+        print("Cal factors are:")
+        print(factorlist)
+        calfactor = sum(factorlist)/len(factorlist)
 
 while True:
     try:
-        filelocation = input("Enter spectra location: ")
-        filelocation = filelocation.strip('"')
+        filelocation = input("Enter spectra location: ").strip('"')
         spectra = np.load(filelocation)
         hist, bins = np.histogram(spectra, bins = spectrarange//20, range = (0, spectrarange))
-        peaks, _ = find_peaks(hist, prominence = int(np.amax(hist))/8)
-        plt.plot(hist, label=filelocation)
-        plt.plot(peaks, hist[peaks], "vk")
+        peaklocations, _ = find_peaks(hist, distance = 10, prominence = int(np.amax(hist))/20)
+        FWHM = peak_widths(hist, peaklocations, rel_height = 0.5)
+        Xenergy = []
+        peaklocations2 = []
+        for x in range(np.size(hist)):
+            Xenergy.append(x/calfactor)
+        for x in range(np.size(peaklocations)):
+            if 5 < FWHM[0][x] < 20:
+                print(str(peaklocations[x]/calfactor) + ' keV | ' + str(hist[int(peaklocations[x])]) + ' counts | ' + str(FWHM[0][x]) + ' FWHM')
+                peaklocations2.append(peaklocations[x])
+        np_peaklocations2 = np.array(peaklocations2, dtype = int)
+        plt.plot(Xenergy, hist, label = filelocation)
+        plt.plot(np_peaklocations2/calfactor, hist[np_peaklocations2], "vk")
     except:
         if filelocation == '':
             break
         else:
             print("Cannot load file")
 
+if calfactor != 1:
+    plt.xlabel('Energy in keV')
 plt.xlim(0)
 plt.legend(loc='upper left')
 plt.show()
